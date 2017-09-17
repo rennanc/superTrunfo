80aed8c5d2fc99a3f6590a4c7db231dca89f487b
Rennan Chagas <rennanchagas@macbook-air-de-renan.local>
2017-09-17 17:36:21 -0300

    add playerMove swift

---
+++ SuperTrunfo/Models/PlayerMove.swift
@@ -0,0 +1,15 @@
+//
+//  PlayerMove.swift
+//  SuperTrunfo
+//
+//  Created by Rennan Chagas on 15/09/17.
+//  Copyright © 2017 Rennan Chagas. All rights reserved.
+//
+
+import UIKit
+
+struct PlayerMove {
+    let image: UIImage!
+    let nameSkill: String!
+    let valueSkill: String!
+}
